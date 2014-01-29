8868225d57ef75878e4d04d887f8662a6ea86183
Macri-man <admfav2012@gmail.com>
2014-01-29 12:22:05 -0500

    Update vertexshader.glsl

--- vertexshader.glsl
+++ vertexshader.glsl
@@ -1,12 +1,10 @@
-in vec3 in_position;
-in vec4 in_color;
 
+in vec3 in_position;
+
+in vec4 in_color;
 out vec4 pass_color;
 
-void main(void)
-{
-
-gl_Position=vec4(in_position, 1.0); 
-
-pass_color=in_color;
+void main(void){
+  gl_Position=vec4(in_position, 1.0); 
+  pass_color=in_color;
 }
