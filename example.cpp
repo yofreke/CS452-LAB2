87e7cc36d3ad9c3f7178c98819eb570a46ddc529
Macri-man <admfav2012@gmail.com>
2014-01-29 23:32:47 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,35 +1,32 @@
 #include "initShaders.h"
 using namespace std;
-int counter=0;
+int counter=0;//counter to increase how many vertices are going to be drawn at a given time
 
-GLuint vaoID, vboID;
+GLuint vaoID, vboID[2];//vao and vbo names
 
-GLfloat vertexarray[]={0.5f,0.5f,0.0f,
-                       0.5f,-0.5f,0.0f,
-                       -0.5f,-0.5f,0.0f,
+GLfloat vertexarray[]={0.5f,0.5f,0.0f,//vertice array
+	1.0f,0.0f,0.0f,
+                       0.5f,-0.5f,0.0f, 
+                       0.0f,-1.0f,0.0f,
+                       -0.5f,-0.5f,0.0f, 
+                        -1.0f,0.0f,0.0f,
                        -0.5f,0.5f,0.0f,
-                       0.0f,0.5f,0.0f,
+                      	0.0f,1.0f,0.0f
                        };
                        
-GLfloat colorarray[]={1.0f,1.0f,0.0f,
-                       0.0f,1.0f,0.0f,
-                       1.0f,1.0f,1.0f,
-                       0.5f,0.5f,1.0f,
-                       1.0f,0.5f,0.0f,
+GLfloat colorarray[]={1.0f,1.0f,0.0f,1.0f,//color array
+                       0.0f,1.0f,0.0f,1.0f,
+                       1.0f,0.0f,1.0f,1.0f,
+                       0.5f,0.5f,1.0f,1.0f,
+                       1.0f,0.5f,0.5f,1.0f,
+                       0.0f,1.0f,0.5f,1.0f,
+                       0.5f,0.5f,0.5f,1.0f,
+                       1.0f,0.5f,1.0f,1.0f
                        };                       
 
 void init(){
 
-  glGenVertexArrays(1, &vaoID);
-  glBindVertexArray(vaoID);
-  
-  glGenBuffers(1, &vboID);
-  glBindBuffer(GL_ARRAY_BUFFER, vboID);
-  glBufferData(GL_ARRAY_BUFFER,sizeof(vertexarray)+sizeof(colorarray),NULL,GL_STATIC_DRAW);
-  glBufferSubData(GL_ARRAY_BUFFER,0,sizeof(vertexarray),vertexarray);
-  glBufferSubData(GL_ARRAY_BUFFER,sizeof(vertexarray),sizeof(colorarray),colorarray);
-
- ShaderInfo shaders[]={
+  ShaderInfo shaders[]={
   { GL_VERTEX_SHADER , "vertexshader.glsl"} ,
   { GL_FRAGMENT_SHADER , "fragmentshader.glsl"},
   { GL_NONE , NULL} 
@@ -37,14 +34,25 @@ void init(){
 
   initShaders(shaders);
   
+  glGenVertexArrays(1, &vaoID);
+  glBindVertexArray(vaoID);
+  
+  glGenBuffers(2, vboID);
+  glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(vertexarray),vertexarray,GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    
+  glBindBuffer(GL_ARRAY_BUFFER, vboID[1]);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(colorarray),colorarray,GL_STATIC_DRAW);
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,vertexarray);
-  glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,colorarray);
+  glEnableVertexAttribArray(1);
 }
 
 void drawscene(){
   glClear(GL_COLOR_BUFFER_BIT);
-  glDrawArrays(GL_TRIANGLES,0,3);
+  glDrawArrays(GL_POLYGON,0,3+(counter%6));
   glFlush();
 }
 
@@ -53,8 +61,7 @@ void mousepress(int button, int state, int x, int y){
     exit(0);
   else if(button==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
     counter++;
-    cout << counter%3 << endl;
-    glutDisplayFunc(drawscene);
+    drawscene();
   }
 }
 
@@ -73,7 +80,8 @@ int main(int argc,char ** argv){
   
   glutInitContextVersion(4, 3);//specifies the version of opengl
   glutInitContextProfile(GLUT_CORE_PROFILE|GLUT_COMPATIBILITY_PROFILE);//specifies what profile your using
-
+	
+	init();
 
   //retruns what version of opengl and glsl your computer can use
   const GLubyte* version=glGetString(GL_SHADING_LANGUAGE_VERSION);
